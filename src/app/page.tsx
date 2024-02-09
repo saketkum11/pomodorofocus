91954a9312d7bc9524fbb5282679b6f93836d0fa
Saket kumar <kumarsaket601@gmail.com>
2024-02-09 22:58:24 +0530

    add: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,71 @@
+"use client";
 import Image from "next/image";
-
+import React, { useState } from "react";
+import { CheckCircleIcon, PlusCircleIcon } from "@heroicons/react/16/solid";
+import {
+  Cog8ToothIcon,
+  UserCircleIcon,
+  EllipsisVerticalIcon,
+} from "@heroicons/react/24/outline";
 export default function Home() {
+  const [toggleSetting, setToggleSetting] = useState(true);
+  function handleClick() {}
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <header className="  bg-red-500 text-white px-3 min-h-screen ">
+        <div className="md:max-w-xl m-auto">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2 py-6 ">
+              <CheckCircleIcon className="w-8 h-8" />
+              <span className="font-semibold text-2xl">Pomodoro App</span>
+            </div>
+            <div>
+              <div className="flex items-center gap-4">
+                <button className="flex items-center justify-center gap-2 bg-red-400 rounded-md p-2 ">
+                  <Cog8ToothIcon className="w-8 " />
+                  <span className="hidden sm:block">Setting</span>
+                </button>
+                <button className="flex items-center justify-center gap-2 bg-red-400 rounded-md p-2">
+                  <UserCircleIcon className="w-8 flex justify-center" />
+                  <span className="hidden sm:block">Login</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col items-center gap-6 bg-red-400 p-14 text-center rounded-md shadow-2xl">
+              <div>
+                <ul className="flex justify-center items-center gap-4">
+                  <li>Pomodoro</li>
+                  <li>Short Break</li>
+                  <li>Long Break</li>
+                </ul>
+              </div>
+              <div className="text-9xl text-center">15:00</div>
+              <button className="bg-white text-red-400 px-8 py-2 rounded-sm">
+                Start
+              </button>
+            </div>
+            <div className="text-center my-4">
+              <p>#4</p>
+              <p className="text-xl">Time for a break!</p>
+            </div>
+            <div className="flex justify-between items-end border-b-2 pb-4">
+              <span className="text-2xl">Tasks</span>
+              <button className="bg-red-400 rounded-md px-2 py-2">
+                <EllipsisVerticalIcon className="w-8" />
+              </button>
+            </div>
+            <button
+              onClick={handleClick}
+              className="flex items-center justify-center w-full gap-4  border-2 border-dashed bg-red-400 py-4 mt-8 shadow-2xl rounded-lg"
+            >
+              <PlusCircleIcon className="w-8" />
+              <span>Add Task</span>
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </header>
     </main>
   );
 }
